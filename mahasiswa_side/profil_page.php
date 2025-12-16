0ef00ceacb3b410f7d58b5e6160e016e310f3a25
Meysa Ramelia Putri <meysaramelia@gmail.com>
2025-12-16 14:28:04 +0700

    commit

--- mahasiswa_side/profil_page.php
+++ mahasiswa_side/profil_page.php
@@ -301,6 +301,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </section>
         </div>
     </main>
+    <!-- Footer -->
+    <footer class="footer-custom">
+        <div class="container">
+            <p class="text-center mb-0">&copy; 2025 Politeknik Negeri Batam - Projek PBL IFPagi 1A-5</p>
+        </div>
+    </footer>
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
