626c5104ad34082f66d9a98a0377e6776da027ca
nabilamarsa56-mrs <nabilamarsa59@gmail.com>
2025-12-17 11:20:47 +0700

    update profil dosen

--- dosen_side/profil_dosen.php
+++ dosen_side/profil_dosen.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['status']) || $_SESSION['status'] != "login" || $_SESSION['
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profil Dosen - WorkPiece</title>
+    <title>Profil Dosen - WorkPiece</title> < --profil dosen-- >
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
