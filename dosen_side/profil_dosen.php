325ca1f32543902c4233f6c1c9282f9474d6d7dc
nabilamarsa56-mrs <nabilamarsa59@gmail.com>
2025-12-19 23:09:02 +0700

    update profil

--- dosen_side/profil_dosen.php
+++ dosen_side/profil_dosen.php
@@ -76,7 +76,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profil Dosen - WorkPiece</title>
+    <title>Profil Dosen - WorkPiece</title> <!--profil dosen-->
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
