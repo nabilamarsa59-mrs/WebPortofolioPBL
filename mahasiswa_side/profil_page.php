f9e5351e8e769927f9846f57870be8ebf22abd77
irenessarosidin62-commits <irenessarosidin62@gmail.com>
2026-01-04 14:29:19 +0700

    update title profil

--- mahasiswa_side/profil_page.php
+++ mahasiswa_side/profil_page.php
@@ -73,7 +73,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profil - WorkPiece</title>
+    <title>Profil Mahasiswa - WorkPiece</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
         rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
