7d315a51b5ceaa356ef88aeaf5cc23fb762d07aa
nabilamarsa56-mrs <nabilamarsa59@gmail.com>
2026-01-06 17:34:44 +0700

    update comments

--- auth_check.php
+++ auth_check.php
@@ -1,28 +1,16 @@
 <?php
-// Memulai session untuk mengakses data login pengguna
 session_start();
-
-// Mengecek apakah user sudah login
 if (!isset($_SESSION['user_id']) || !isset($_SESSION['role'])) {
     header("Location: ../login.php"); 
     exit(); 
 }
 
-// Menyimpan lokasi file PHP yang sedang diakses oleh user
  $current_file = $_SERVER['PHP_SELF'];
-
- // Mengecek akses halaman dosen
-// Jika user membuka folder dosen_side tetapi rolenya bukan dosen,
-// maka user akan diarahkan kembali ke halaman login
 if (strpos($current_file, '/dosen_side/') !== false && $_SESSION['role'] !== 'dosen') {
     header("Location: ../login.php");
     exit();
 }
 
-
-// Mengecek akses halaman mahasiswa
-// Jika user membuka folder mahasiswa_side tetapi rolenya bukan mahasiswa,
-// maka user akan diarahkan kembali ke halaman login
 if (strpos($current_file, '/mahasiswa_side/') !== false && $_SESSION['role'] !== 'mahasiswa') {
     header("Location: ../login.php");
     exit();
--- logout.php
+++ logout.php
@@ -1,13 +1,6 @@
 <?php
-// Memulai session agar session yang aktif bisa diakses
 session_start();
-
-// Menghapus seluruh data session (logout user)
 session_destroy();
-
-// Mengarahkan user kembali ke halaman login setelah logout
 header("Location: login.php");
-
-// Menghentikan eksekusi script agar redirect berjalan sempurna
 exit();
 ?>
